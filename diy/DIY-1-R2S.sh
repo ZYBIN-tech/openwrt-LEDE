f8730f1fe87ffcdfd3da020558fbf2753437eda1
ZYBIN-tech <67323819+ZYBIN-tech@users.noreply.github.com>
2024-08-07 11:52:23 +0800

    0807

---
+++ diy/DIY-1-R2S.sh
@@ -0,0 +1,9 @@
+# 此脚本用处是：添加第三方插件
+#=========================================================================================================================
+# 自用插件源
+echo "src-git danshui1 https://github.com/281677160/openwrt-package.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git passwall1 https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> "feeds.conf.default"
+echo "src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages" >> "feeds.conf.default"
+echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master" >> "feeds.conf.default"
